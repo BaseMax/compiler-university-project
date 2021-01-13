130dcbff30f8cf43709358bea26b3b484020b10a
Max Base <MaxBaseCode@Gmail.Com>
2021-01-13 21:19:07 +0330

    compile cpp gen and run for test purpose

--- build.sh
+++ build.sh
@@ -1,3 +1,14 @@
+# parser
 bison -d $2 parser.y -Wcounterexamples
+# bison -d $2 parser.y
+
+# lexer
 flex $1 lexer.l
+
+# bind
 gcc -o parser lex.yy.c parser.c sds.c
+
+# compile
+g++ test.cpp -o test
+# run/test
+./test
